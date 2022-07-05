b78b362e5104924d463fe190e621e55e3bfc0262
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-05 18:56:40 +0530

    Added: Password to user registration program

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -33,6 +33,11 @@ public class UserRegistration {
         System.out.println(isValidEmail.validate("^[a-z]+[+-_.]*[a-z]*[@][a-z]+[.][a-z]{2,4}[.]*([a-z]{2})*$",
                 "nagaraja09@gmail.com"));
 
+        IValidation isValidPassword1 = (pattern, password) -> {
+            return "Password rule is " + Pattern.compile(pattern).matcher(password).matches();
+        };
+        System.out.println(isValidPassword1.validate("^[0-9a-zA-Z!,@#$&*().]{8,}$", "Nsg@Sgns"));
+
 
 
     }
