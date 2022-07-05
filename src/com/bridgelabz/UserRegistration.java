f77f9926586f7bccb09957ac31abbb493a831442
nagarajnsg1995 <nagarajsbg023@gmail.com>
2022-07-05 18:24:33 +0530

    Added: First name to user registration

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,9 +1,20 @@
 package com.bridgelabz;
 
-public class UserRegistration {
-    public static void main(String[] args) {
+import java.util.regex.Pattern;
 
+interface IValidation {
+    String validate(String regex, String userDetails);
+}
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
         System.out.println("Welcome to lambda user registration program");
+
+        IValidation isValidFirstName = (pattern, firstName) -> {
+            return "First Name is " + Pattern.compile(pattern).matcher(firstName).matches();
+        };
+        System.out.println(isValidFirstName.validate("^[A-Z]{1}[a-z]{2,}$", "Nagaraj"));
 
     }
 }
